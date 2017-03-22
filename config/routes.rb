4e044af0d5696559d45966e1a734f628fc1dd66b
ryuto <tun_tnd@i.softbank.jp>
2017-03-23 01:51:50 +0900

    fixes config

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
   root to: 'static_pages#home'
   get '/sign_up', to: 'static_pages#signup'
   get '/sign_in', to: 'sessions#new'
+  get 'workers/:token/activate', to: 'workers#activate', as: 'activate_worker'
   post '/sign_in', to: 'sessions#create'
   delete '/sign_out', to: 'sessions#destroy'
 
-  resources :workers, except: %i(new), param: :workername, path: '/' do
+  resources :workers, except: %i(new), param: :username, path: '/' do
     get '/create_profile', to: 'profiles#new'
-    resource :profiles, only: %i(create), path: '/' do
+    resource :profiles, only: %i(create), path: '/', controller: :worker_profiles do
       scope '/profile' do
         member do
           get :confirm
@@ -16,14 +17,12 @@ Rails.application.routes.draw do
     end
   end
 
-
-  resources :clients, except: %i(new), param: :clientname, path: '/' do
+  resources :clients, except: %i(new), param: :username, path: '/' do
     get '/create_profile', to: 'profiles#new'
     get '/create_job', to: 'jobs#new'
-    resource :profiles, only: %i(create)
-    resources :jobs, only: %i(show create update destroy)
+    resource :profiles, only: %i(create), controller: :client_profiles
+    resources :jobs, only: %i(show create update destroy), controller: :client_jobs
   end
-
 
   scope '/worker' do
     get '/sign_up', to: 'workers#new'
