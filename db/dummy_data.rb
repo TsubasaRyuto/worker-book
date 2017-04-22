a6cf272db01b8c4c643dbeed5cbf016d28ea1f20
ryuto <tun_tnd@i.softbank.jp>
2017-04-22 18:25:34 +0900

    add dummy data

---
+++ db/dummy_data.rb
@@ -0,0 +1,57 @@
+def clean_data
+  Worker.delete_all
+  WorkerProfile.delete_all
+  WorkerSkill.delete_all
+end
+
+def workers
+  2.times do |index|
+    name = Faker::Name
+    last_name = name.last_name
+    first_name = name.first_name
+    availability = WorkerProfile.availabilities.keys.sample
+    note = Faker::Lorem.sentence(400)
+    picture = Rails.root.join('spec/fixtures/images/lobo.png').open
+
+    worker = Worker.create(
+      last_name: last_name,
+      first_name: first_name,
+      username: "example_worker#{index + 1}",
+      email: "example#{index + 1}@example.com",
+      password: 'foobar123',
+      password_confirmation: 'foobar123',
+      activated: true
+    )
+    worker_profile = worker.build_profile(
+      type_web_developer: true,
+      type_mobile_developer: true,
+      availability: availability,
+      past_performance1: 'http://example1.com',
+      past_performance2: 'http://example2.com',
+      unit_price: 40000,
+      appeal_note: note,
+      picture: picture,
+      location: 02,
+      employment_history1: 'Lobo 株式会社'
+    )
+    worker_profile.save
+  end
+end
+
+def worker_skills
+  workers = Worker.all
+  skills = %w(1 2 3 4 5)
+  worker_skills = []
+
+  workers.each do |worker|
+    skills.each do |skill|
+      worker_skill = worker.worker_skills.build(skill_language_id: skill)
+      worker_skills.push(worker_skill)
+    end
+    worker_skills.each(&:save)
+  end
+end
+
+clean_data
+workers
+worker_skills
