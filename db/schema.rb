18c50b95ffe83e22890a388a2579311cdbca045b
ryuto <tun_tnd@i.softbank.jp>
2017-05-23 10:12:26 +0900

    update gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170429190849) do
+ActiveRecord::Schema.define(version: 20170523005906) do
 
   create_table "agreements", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "worker_id",                     null: false
