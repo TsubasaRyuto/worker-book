7576aea471459854d45796127a184344284313de
ryuto <tun_tnd@i.softbank.jp>
2017-06-20 17:22:17 +0900

    fixes

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :puma_threads,    [4, 16]
 set :puma_workers,    0
 set :deploy_to, '/var/www/workerbook'
 
-set :puma_bind,       "unix:/#{shared_path}/tmp/sockets/puma.sock"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
